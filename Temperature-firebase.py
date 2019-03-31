adb4db0ae3129ba4d7b32d709519a930a94863f2
Gaurav Kalsi <franko.kalsi@gmail.com>
2019-03-31 17:00:25 -0400

    HOST ID and Project Name in comments

--- Temperature-firebase.py
+++ Temperature-firebase.py
@@ -10,6 +10,6 @@ while True:
 
     print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} ".format(temperature, humidity))
 
-    firebase = firebase.FirebaseApplication('HOST ID') #HOST ID from FIREBASE
-    result = firebase.post('Project Name', {'temp':str(temperature), 'humidity':str(humidity)}) #Project Name from FIREBASE
+    firebase = firebase.FirebaseApplication('HOST ID') #HOST ID from FIREBASE 'https://firefightingmonitoringsystem.firebaseio.com/'
+    result = firebase.post('Project Name', {'temp':str(temperature), 'humidity':str(humidity)}) #Project Name from FIREBASE 'firefightingmonitoringsystem'
     print(result)
