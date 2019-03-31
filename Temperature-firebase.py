668677f1bfd74576f118083a85c861f300351c1b
Gaurav Kalsi <franko.kalsi@gmail.com>
2019-03-31 16:46:54 -0400

    Add files via upload
    Connection to Firebase added.

---
+++ Temperature-firebase.py
@@ -0,0 +1,15 @@
+import sys
+import Adafruit_DHT
+from firebase import firebase
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    
+    sensor = Adafruit_DHT.read(11,4)
+
+    print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} ".format(temperature, humidity))
+
+    firebase = firebase.FirebaseApplication('HOST ID') #HOST ID from FIREBASE
+    result = firebase.post('Project Name', {'temp':str(temperature), 'humidity':str(humidity)}) #Project Name from FIREBASE
+    print(result)
