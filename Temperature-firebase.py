39aed02c5af9a2b30c8d5efcb9106ab8dc7657e3
Gaurav Kalsi <franko.kalsi@gmail.com>
2019-04-01 12:16:31 -0400

    Added Keys.

--- Temperature-firebase.py
+++ Temperature-firebase.py
@@ -10,11 +10,11 @@ while True:
 
     print ("Temp: {0:0.1f} C  Humidity: {1:0.1f} ".format(temperature, humidity))
 
-    config = {"apiKey": "",
-              "authDomain": ".firebaseapp.com",
-              "databaseURL": ".firebaseio.com",
-              "storageBucket": ".appspot.com"
-    
+    config = {"apiKey": "AIzaSyA20qu9ddnRJPAQgGpn9ySQLuqjLH2WWPI",
+              "authDomain": "firefightingmonitoringsystem.firebaseapp.com",
+              "databaseURL": "https://firefightingmonitoringsystem.firebaseio.com/",
+              "storageBucket": "firefightingmonitoringsystem.appspot.com"
+             }
     fireBase = pyrebase.initialize_app(config)
     .FirebaseApplication('https://firefightingmonitoringsystem.firebaseio.com/') #HOST ID from FIREBASE 'https://firefightingmonitoringsystem.firebaseio.com/'
     result = firebase.post('firefightingmonitoringsystem', {'temp':str(temperature), 'humidity':str(humidity)}) #Project Name from FIREBASE 'firefightingmonitoringsystem'
