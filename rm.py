51943c496aac0f32134f37ac4b09848bf8bc7bc6
Gaurav Kalsi <franko.kalsi@gmail.com>
2019-11-14 12:39:14 -0500

    Proper formatting

--- rm.py
+++ rm.py
@@ -6,7 +6,16 @@ import json
 import re
 from pyrebase import pyrebase
 
+config = {"apiKey": "AIzaSyA20qu9ddnRJPAQgGpn9ySQLuqjLH2WWPI",
+              "authDomain": "firefightingmonitoringsystem.firebaseapp.com",
+              "databaseURL": "https://firefightingmonitoringsystem.firebaseio.com/",
+              "storageBucket": "firefightingmonitoringsystem.appspot.com"
+             }
+fireBase = pyrebase.initialize_app(config)
+db = fireBase.database() 
+db.remove() #removes previous data
 
+usrs = ["usr0", "usr1", "usr2", "usr3"]
 while True:
     hum1 = random.randint(30,40)
     hum2 = random.randint(30,40)
@@ -28,48 +37,49 @@ while True:
     hartbt3 = random.randint(70,100)
     hartbt4 = random.randint(70,100)
 
-    config = {"apiKey": "AIzaSyA20qu9ddnRJPAQgGpn9ySQLuqjLH2WWPI",
-              "authDomain": "firefightingmonitoringsystem.firebaseapp.com",
-              "databaseURL": "https://firefightingmonitoringsystem.firebaseio.com/",
-              "storageBucket": "firefightingmonitoringsystem.appspot.com"
-             }
-    fireBase = pyrebase.initialize_app(config)
 
-    db = fireBase.database()
+    timestamp = datetime.datetime.now().time().isoformat()
     dt = str(datetime.datetime.now().date()) + "_" + str(datetime.datetime.now().time().isoformat())
     dt = re.sub("\.", "_", dt)
     print ("Time pushed: " + dt)
 
-    data = {
-        "users":{
-            "arsham":{
-                "chestTemperature": chestTemp1,
-                "externalTemperature": extTemp1,
-                "humidity": hum1,
-                "heartbeat": hartbt1,
-                },
-            "filip":{
-                "chestTemperature": chestTemp2,
-                "externalTemperature": extTemp2,
-                "humidity": hum2,
-                "heartbeat": hartbt2,
-                },
-            "franko":{
-                "chestTemperature": chestTemp3,
-                "externalTemperature": extTemp3,
-                "humidity": hum3,
-                "heartbeat": hartbt3,
-                },
-            "yuhan":{
-                "chestTemperature": chestTemp4,
-                "externalTemperature": extTemp4,
-                "humidity": hum4,
-                "heartbeat": hartbt4,
-                },
-            }
+    data1 = {
+            dt:{
+                "chestTemperature":chestTemp1,
+                "externalTemperature":extTemp1,
+                "heartRate":hartbt1,
+                "humidity":hum1,
+            },
+    }
+    data2 = {
+            dt:{
+                "chestTemperature":chestTemp2,
+                "externalTemperature":extTemp2,
+                "heartRate":hartbt2,
+                "humidity":hum2,
+            },
+    }
+    data3 = {
+            dt:{
+                "chestTemperature":chestTemp3,
+                "externalTemperature":extTemp3,
+                "heartRate":hartbt3,
+                "humidity":hum3,
+            },
+    }
+    data4 = {
+            dt:{
+                "chestTemperature":chestTemp4,
+                "externalTemperature":extTemp4,
+                "heartRate":hartbt4,
+                "humidity":hum4,
+            },
     }
 
-    result = db.child("pi_data").child(dt).set(data)
+    result = db.child("pi_data/users/usr1").update(data1)
+    result = db.child("pi_data/users/usr2").update(data2)
+    result = db.child("pi_data/users/usr3").update(data3)
+    result = db.child("pi_data/users/usr4").update(data4)
     print("Res: " + str(result))
 
-    time.sleep(5) #5 second works?
+    time.sleep(2) #5 second works?
