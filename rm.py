98f2c55cd305252dc790244d08d5ff30dc8d7d29
Gaurav Kalsi <franko.kalsi@gmail.com>
2019-11-16 20:16:20 -0500

    Added GPS data

--- rm.py
+++ rm.py
@@ -15,7 +15,7 @@ fireBase = pyrebase.initialize_app(config)
 db = fireBase.database() 
 db.remove() #removes previous data
 
-usrs = ["usr0", "usr1", "usr2", "usr3"]
+# usrs = ["usr0", "usr1", "usr2", "usr3"]
 while True:
     hum1 = random.randint(30,40)
     hum2 = random.randint(30,40)
@@ -37,6 +37,15 @@ while True:
     hartbt3 = random.randint(70,100)
     hartbt4 = random.randint(70,100)
 
+    gpsN1 = round(random.uniform(45.0,45.9),6)
+    gpsW1 = round(random.uniform(75.0,75.9),6)
+    gpsN2 = round(random.uniform(45.0,45.9),6)
+    gpsW2 = round(random.uniform(75.0,75.9),6)
+    gpsN3 = round(random.uniform(45.0,45.9),6)
+    gpsW3 = round(random.uniform(75.0,75.9),6)
+    gpsN4 = round(random.uniform(45.0,45.9),6)
+    gpsW4 = round(random.uniform(75.0,75.9),6)
+
 
     timestamp = datetime.datetime.now().time().isoformat()
     dtD = str(datetime.datetime.now().date()) + "_" + str(datetime.datetime.now().time().isoformat())
@@ -51,6 +60,8 @@ while True:
                 "externalTemperature":extTemp1,
                 "heartRate":hartbt1,
                 "humidity":hum1,
+                "gpsN":gpsN1,
+                "gpsW":gpsW1,
                 "dateTime": timestamp,
             },
     }
@@ -60,6 +71,8 @@ while True:
                 "externalTemperature":extTemp2,
                 "heartRate":hartbt2,
                 "humidity":hum2,
+                "gpsN":gpsN2,
+                "gpsW":gpsW2,
                 "dateTime": timestamp,
             },
     }
@@ -69,6 +82,8 @@ while True:
                 "externalTemperature":extTemp3,
                 "heartRate":hartbt3,
                 "humidity":hum3,
+                "gpsN":gpsN3,
+                "gpsW":gpsW3,
                 "dateTime": timestamp,
             },
     }
@@ -78,14 +93,16 @@ while True:
                 "externalTemperature":extTemp4,
                 "heartRate":hartbt4,
                 "humidity":hum4,
+                "gpsN":gpsN4,
+                "gpsW":gpsW4,
                 "dateTime": timestamp,
             },
     }
 
-    result = db.child("pi_data/users/usr1").update(data1)
+    result1 = db.child("pi_data/users/usr1").update(data1)
     result2 = db.child("pi_data/users/usr2").update(data2)
     result3= db.child("pi_data/users/usr3").update(data3)
     result4 = db.child("pi_data/users/usr4").update(data4)
-    print("Res: " + str(result))
+    print("Res:\n{}\n{}\n{}\n{}\n".format(str(result1),str(result2),str(result3),str(result4)))
 
     time.sleep(5) #5 second works?
